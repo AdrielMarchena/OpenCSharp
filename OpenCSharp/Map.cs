cdb7fd299b4877c8d006874f7aad586b36cfb23a
AdrielMarchena <Adriel_contato@hotmail.com>
2021-03-01 12:41:58 -0300

    Map added

---
+++ OpenCSharp/Map.cs
@@ -0,0 +1,122 @@
+﻿using GlmNet;
+using Engine.render;
+
+namespace OpenCSharp
+{
+    public enum TileType : int
+    {
+        INVALID = '0',
+        OUTBOUND = 'O',
+        AIR = 'A',
+        GROUND = 'G',
+        WALL = 'W'
+    }
+
+    public struct Tile
+    {
+        public bool IsSolid;
+        public Entity Block;
+        public TileType Type;
+    }
+
+    public class Map
+    {
+        public vec2 PlayerInitPos { get; protected set; }
+        public vec2 MapSize;
+        public Tile[,] TileMap;
+
+        public Map(uint w, uint h, string MapLayout, uint tileSize = 64)
+        {
+            TileMap = new Tile[w, h];
+            MapSize = new vec2(w, h);
+            var A = Window.m_subTexs[SubTex.Invalid];
+            var G = Window.m_subTexs[SubTex.Ground1];
+            var W = Window.m_subTexs[SubTex.Wall1];
+            int lineh = ((int)h * (int)w) - (int)w;
+            int hI = 0;
+            Entity tmp;
+            for (int i = (int)h; i > 0; i--)
+            {
+                string line = MapLayout.Substring(lineh, (int)w);
+
+                for (int j = 0; j < w ; j++)
+                {
+                    switch (line[j])
+                    {
+                        case 'G':
+                            TileMap[j, hI].IsSolid = true;
+                            TileMap[j, hI].Type = TileType.GROUND;
+                            tmp = new Entity(G)
+                            {
+                                DrawIt = true,
+                                UpdateIt = true,
+                                Position = new vec2(j * tileSize, hI * tileSize),
+                                Size = new vec2(tileSize)
+                            };
+                            TileMap[j, hI].Block = tmp;
+                        break;
+                        case 'W':
+                            TileMap[j, hI].IsSolid = true;
+                            TileMap[j, hI].Type = TileType.WALL;
+                            tmp = new Entity(W)
+                            {
+                                DrawIt = true,
+                                UpdateIt = true,
+                                Position = new vec2(j * tileSize, hI * tileSize),
+                                Size = new vec2(tileSize)
+                            };
+                            TileMap[j, hI].Block = tmp;
+                        break;
+                        case 'A':
+
+                            TileMap[j, hI].IsSolid = false;
+                            TileMap[j, hI].Type = TileType.AIR;
+                            tmp = new Entity(A)
+                            {
+                                DrawIt = false,
+                                UpdateIt = true,
+                                Position = new vec2(j * tileSize, hI * tileSize),
+                                Size = new vec2(tileSize)
+                            };
+                            TileMap[j, hI].Block = tmp;
+
+                        break;
+                    }
+                }
+                hI++;
+                lineh -= (int)w;
+            }
+        }
+        public void Draw()
+        {
+
+            for (int i = 0; i < MapSize.y; i++)
+            {
+                for (int j = 0; j < (int)MapSize.x; j++)
+                {
+                    var tile = TileMap[j, i];
+                    if (!tile.Block.DrawIt)
+                        continue;
+                    Render2D.DrawQuad(tile.Block.Position, tile.Block.Size, tile.Block.STexture);
+                }
+            }
+        }
+
+        public Tile WhatIsHere(uint w, uint h)
+        {
+            return TileMap[w, h];
+        }
+
+        public Tile this[uint w, uint h]
+        {
+            get { return TileMap[w,h]; }
+            protected set { TileMap[w,h] = value; }
+        }
+
+        public bool IsDone()
+        {
+            return false;
+        }
+
+    }
+}
--- OpenCSharp/Window.cs
+++ OpenCSharp/Window.cs
@@ -2,7 +2,6 @@
 using OpenTK.Windowing.GraphicsLibraryFramework;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Graphics.OpenGL;
-using OpenTK.Mathematics;
 using Engine.render;
 using Engine.camera;
 using System;
@@ -14,6 +13,9 @@ using Engine.audio;
 
 namespace OpenCSharp
 {
+    /// <summary>
+    /// Index for Subtextures(Yes, this is pretty static i know)
+    /// </summary>
     public enum SubTex : byte
     {
         Invalid = 0,
@@ -27,67 +29,75 @@ namespace OpenCSharp
         Wall2,
         Wall3
     }
+
+    public enum GameState
+    {
+        Running,
+        Pause,
+        Exit
+    }
+
     public class Window : GameWindow
 
     {
         private Shader shader;
+        /// <summary>
+        /// Global camera
+        /// </summary>
         public static readonly OrthographicCameraController camera;
-        static private vec2 ScreenSize;
+        /// <summary>
+        /// It's updated when the screen is resized
+        /// </summary>
+        static public vec2 ScreenSize { get; protected set; }
+        static public MouseState mouse { get; protected set; }
+        static public KeyboardState keyboard { get; protected set; }
 
-        static private readonly ResourceManager<SubTex, SubTexture> m_subTexs = new ResourceManager<SubTex, SubTexture>();
-        static private readonly ResourceManager<string, Texture> m_textures = new ResourceManager<string, Texture>();
-        static private readonly ResourceManager<string, Text> m_fonts = new ResourceManager<string, Text>();
-        static private readonly ResourceManager<string, SoundPlayer> m_snds = new ResourceManager<string, SoundPlayer>();
-
-        private readonly unsafe List<Entity> m_entities;
+        /// <summary>
+        /// Static SubTextures from a Atlas Texture
+        /// </summary>
+        static public readonly ResourceManager<SubTex, SubTexture> m_subTexs = new ResourceManager<SubTex, SubTexture>();
+        static public readonly ResourceManager<string, Texture> m_textures = new ResourceManager<string, Texture>();
+        static public readonly ResourceManager<string, Text> m_fonts = new ResourceManager<string, Text>();
+        static public readonly ResourceManager<string, SoundPlayer> m_snds = new ResourceManager<string, SoundPlayer>();
+        /// <summary>
+        /// Entity list,
+        /// </summary>
+        private readonly List<Entity> m_entities;
 
         static private readonly SoundPlayer mPlayer = new SoundPlayer();
 
         private readonly ParticlesSystem emiter;
         private ParticleProps props;
 
+        private Map map;
+
+        /// <summary>
+        /// Create all those SubTex from the TexMap
+        /// </summary>
         static private void montSubText()
         {
-            var Map = m_textures.GetResource("MapTextures");
-            vec2 ImageSize = new vec2(Map.Width, Map.Height);
+            var MapT = m_textures.GetResource("MapTextures");
+            vec2 ImageSize = new vec2(MapT.Width, MapT.Height);
             vec2 SpriteSize = new vec2(64);
-            
+
             //Default subtex for invalid itens
-            SubTexture tmp = SubTexture.CreateFromCoords(Map.textureID, ImageSize, new vec2(0, 0), new vec2(1,1));
+            SubTexture tmp = SubTexture.CreateFromCoords(MapT.textureID, ImageSize, new vec2(0, 0), new vec2(1,1));
             m_subTexs[SubTex.Invalid] = tmp;
             //Get sub texts from the Major tex here
             int i = 1;
             for(int r = 3; r >= 0; r--)
             {
-                
                 for(int c = 0; c <= 3;c++)
                 {
-                    tmp = SubTexture.CreateFromCoords(Map.textureID, ImageSize, new vec2(c , r), SpriteSize);
+                    tmp = SubTexture.CreateFromCoords(MapT.textureID, ImageSize, new vec2(c , r), SpriteSize);
                     m_subTexs[(SubTex)i] = tmp;
                     i++;
                 }
             }
-
-           /*SubTexture tmp = SubTexture.CreateFromCoords(Map.textureID, ImageSize, new vec2(0,3), SpriteSize);
-            m_subTexs.AddResouce("player_tex", tmp);
-
-            tmp = SubTexture.CreateFromCoords(Map.textureID, ImageSize, new vec2(1,3) ,SpriteSize);
-            m_subTexs.AddResouce("enemy_tex", tmp);
-
-            tmp = SubTexture.CreateFromCoords(Map.textureID, ImageSize, new vec2(2, 3), SpriteSize);
-            m_subTexs.AddResouce("item_tex", tmp);
-
-            tmp = SubTexture.CreateFromCoords(Map.textureID, ImageSize, new vec2(3, 3), SpriteSize);
-            m_subTexs.AddResouce("ground_tex", tmp);
-
-            tmp = SubTexture.CreateFromCoords(Map.textureID, ImageSize, new vec2(0, 2), SpriteSize);
-            m_subTexs.AddResouce("ground2_tex", tmp);
-
-            tmp = SubTexture.CreateFromCoords(Map.textureID, ImageSize, new vec2(1, 2), SpriteSize);
-            m_subTexs.AddResouce("ground3_tex", tmp);*/
-
         }
-
+        /// <summary>
+        /// Load Resources Here
+        /// </summary>
         static private void Load_resources()
         {
             m_textures.AddResource("test", new Texture("tex/Test.png"));
@@ -96,6 +106,7 @@ namespace OpenCSharp
             m_fonts.AddResource("Arial", new Text("fonts/arial.ttf"));
             //Play on My headphone, the default is 0
             mPlayer.Open("snd/Requiem.mp3", SoundPlayer.Devices[1]);
+            mPlayer.Volume = 2;
             m_snds.AddResource("music", mPlayer);
         }
 
@@ -105,7 +116,20 @@ namespace OpenCSharp
             Load_resources();
             montSubText();
             m_entities = new List<Entity>();
-            
+
+            string layout = "AAAAAAAAAAAAAAAAAAAA" +
+                            "AAAAAAAAAAAAAAAAAAAA" +
+                            "AAAAAAAAAAAAAAAAAAAA" +
+                            "AAAAAAAAAAAAAAAAAAAW" +
+                            "WAAAAAAWWWWAAAAAAAAA" +
+                            "WAAAAAAAAAAAAAAAAAAA" +
+                            "WAAAAAAAAAAAAAAAAAAA" +
+                            "GGGGGGGGGGGGGGAAAGGG" +
+                            "GAAGGAAAAGGAAAAAAAAG" +
+                            "WGGGGGGGGGGGGGGGGGGG";
+
+            map = new Map(20, 10, layout);
+
             ScreenSize = new vec2(1024.0f, 576.0f);
 
             props = ParticleProps.Effect2;
@@ -145,7 +169,7 @@ namespace OpenCSharp
             shader.SetUniformMat4("u_Transform", glm.ortho(0,ScreenSize.x ,0,ScreenSize.y,-1.0f,100.0f).to_array());
             
             TextRender.Init();
-            TextRender.SetProjView(camera.GetCamera().GetViewProjectionMatrixArray());
+            TextRender.SetProjView(mat4.identity().to_array());
             TextRender.SetTransform(glm.ortho(0, ScreenSize.x, 0, ScreenSize.y, -1.0f, 100.0f).to_array());
 
             PlayerTest player = new PlayerTest(m_textures.PopResource("test"));
@@ -157,24 +181,16 @@ namespace OpenCSharp
 
             m_snds.GetResource("music").Play();
 
-
             base.OnLoad();
         }
 
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
             if(KeyboardState.IsKeyDown(Keys.Escape))
-            {
                 Close();
-            }
 
-            if(MouseState.IsButtonDown(MouseButton.Button1))
-            {
-                props.Position.y = camera.GetCamera().GetPosition().y - (MousePosition.Y - ScreenSize.y);
-                props.Position.x = MousePosition.X;
-                emiter.Emit(props);
-                m_snds.GetResource("music").Play();
-            }else m_snds.GetResource("music").Pause();
+            mouse = MouseState;
+            keyboard = KeyboardState;
 
             camera.OnUpdate(KeyboardState, e.Time);
             
@@ -182,9 +198,7 @@ namespace OpenCSharp
             shader.SetUniformMat4("u_ViewProj", camera.GetCamera().GetViewProjectionMatrixArray());
             shader.SetUniformMat4("u_Transform", glm.ortho(0, ScreenSize.x, 0, ScreenSize.y, -1.0f, 100.0f).to_array());
 
-            TextRender.SetProjView(mat4.identity().to_array());
             TextRender.SetTransform(glm.ortho(0, ScreenSize.x, 0, ScreenSize.y, -1.0f, 100.0f).to_array());
-
 
             foreach (Entity en in m_entities)
             {
@@ -197,51 +211,23 @@ namespace OpenCSharp
             base.OnUpdateFrame(e);
         }
 
-        protected override void OnMouseWheel(MouseWheelEventArgs e)
-        {
-            camera.OnMouseScrolled(e.OffsetY);
-            base.OnMouseWheel(e);
-        }
         protected override void OnRenderFrame(FrameEventArgs args)
         {
             GL.Clear(ClearBufferMask.ColorBufferBit);
             shader.Bind();
             Render2D.BeginBatch();
+
             //Draws here
-            vec2 PlayerPos = new vec2(1);
-            float pos = 150.0f;
 
-            m_fonts["Arial"].RenderText("Player: ", pos, 80.0f, 0.5f, new vec3(0.8f, 0.8f, 0.9f));
-            Render2D.DrawQuad(new vec2(pos, 10.0f), new vec2(50.0f, 50.0f), m_subTexs[SubTex.Player]);
-            pos += 100.0f;
-
-            m_fonts["Arial"].RenderText("Enemy: ", pos, 80, 0.5f, new vec3(0.8f, 0.8f, 0.9f));
-            Render2D.DrawQuad(new vec2(pos, 10.0f), new vec2(50.0f, 50.0f), m_subTexs[SubTex.Enemy]);
-            pos += 100.0f;
-
-            m_fonts["Arial"].RenderText("Item: ", pos, 80, 0.5f, new vec3(0.8f, 0.8f, 0.9f));
-            Render2D.DrawQuad(new vec2(pos, 10.0f), new vec2(50.0f, 50.0f), m_subTexs[SubTex.Item]);
-            pos += 100.0f;
-
-            m_fonts["Arial"].RenderText("Ground: ", pos , 80, 0.5f, new vec3(0.8f, 0.8f, 0.9f));
-            Render2D.DrawQuad(new vec2(pos, 10.0f), new vec2(50.0f, 50.0f), m_subTexs[SubTex.Ground1]);
-            pos += 100.0f;
-
-
+            map.Draw();
             foreach (Entity en in m_entities)
             {
                 if (en.DrawIt)
                     en.OnRender(args);
-                PlayerPos = en.Position;
             }
-            
-            double Distance = Math.Sqrt(Math.Pow(PlayerPos.x - 50.0f,2) + Math.Pow(50.0f - PlayerPos.y,2));
-            double fps = RenderTime + UpdateTime / 2 * 1000;
+            //vec2 PlayerPos = new vec2(1);
+            //double Distance = Math.Sqrt(Math.Pow(PlayerPos.x - 50.0f,2) + Math.Pow(50.0f - PlayerPos.y,2));
 
-            m_fonts.GetResource("Arial").RenderText("distance is: " + Distance.ToString(), 300.0f, 375.0f, 0.5f, new vec3( 0.8f, 0.8f,0.9f));
-            m_fonts.GetResource("Arial").RenderText("Time Elapsed: " + args.Time, 50.0f, ScreenSize.y - 50.0f, 0.5f, new vec3(0.2f, 0.5f, 0.8f));
-            m_fonts.GetResource("Arial").RenderText("FPS: " + fps, 50.0f, ScreenSize.y - 100.0f, 0.5f, new vec3(0.2f, 0.5f, 0.8f));
-            
             emiter.OnRender();
 
             Render2D.EndBatch();
@@ -249,6 +235,11 @@ namespace OpenCSharp
 
             Context.SwapBuffers();
             base.OnRenderFrame(args);
+        }
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            camera.OnMouseScrolled(e.OffsetY);
+            base.OnMouseWheel(e);
         }
 
         protected void PushEntity(ref Entity ent)
